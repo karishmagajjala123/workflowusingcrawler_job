242566c6bf76ea9fb253ea10e337a8bda2d074b8
karishmagajjala123 <134476356+karishmagajjala123@users.noreply.github.com>
2023-07-10 12:08:03 +0530

    Update workflowusingcrawler_ job.py

--- workflowusingcrawler_ job.py
+++ workflowusingcrawler_ job.py
@@ -4,6 +4,7 @@ from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
+import re
 
 args = getResolvedOptions(sys.argv, ["JOB_NAME"])
 sc = SparkContext()
@@ -23,23 +24,26 @@ ApplyMapping_node2 = ApplyMapping.apply(
     mappings=[
         ("rank", "long", "rank", "int"),
         ("title", "string", "title", "string"),
-        ("genre", "string", "genre", "string"),
-        ("description", "string", "description", "string"),
         ("director", "string", "director", "string"),
         ("actors", "string", "actors", "string"),
         ("year", "long", "year", "int"),
-        ("`runtime (minutes)`", "long", "`runtime (minutes)`", "int"),
         ("rating", "double", "rating", "double"),
         ("votes", "long", "votes", "int"),
         ("`revenue (millions)`", "double", "`revenue (millions)`", "double"),
-        ("metascore", "long", "metascore", "long"),
     ],
     transformation_ctx="ApplyMapping_node2",
 )
 
+# Script generated for node Filter
+Filter_node1688969673535 = Filter.apply(
+    frame=ApplyMapping_node2,
+    f=lambda row: (row["revenue (millions)"] > 300),
+    transformation_ctx="Filter_node1688969673535",
+)
+
 # Script generated for node S3 bucket
 S3bucket_node3 = glueContext.write_dynamic_frame.from_options(
-    frame=ApplyMapping_node2,
+    frame=Filter_node1688969673535,
     connection_type="s3",
     format="glueparquet",
     connection_options={"path": "s3://04072023/output/", "partitionKeys": []},
